d63ff8959e65687fcb8904445b90d4cb4dc06ac4
IT Multifab <itmultifab@gmail.com>
2026-08-31 13:17:55 +0700

    Fix: CORS - gunakan regex pattern untuk semua port localhost

--- config/cors.php
+++ config/cors.php
@@ -5,9 +5,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5174', 'https://medicore-clinic.vercel.app'],
+    'allowed_origins' => ['https://medicore-clinic.vercel.app'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^http://localhost:[0-9]+$#',
+        '#^http://127\.0\.0\.1:[0-9]+$#',
+        '#^http://192\.168\.[0-9]+\.[0-9]+:[0-9]+$#',
+    ],
 
     'allowed_headers' => ['*'],
 
